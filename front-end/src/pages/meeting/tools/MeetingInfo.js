8f09f6d56114eca0546ed22f66c35aafee92dd61
chenhaolu0506 <47787200+chenhaolu0506@users.noreply.github.com>
2025-05-11 22:38:54 -0500

    meeting drawer

---
+++ front-end/src/pages/meeting/tools/MeetingInfo.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import { Table, Button, message, Tooltip, Input } from "antd";
+import global from "../../../global";
+import '../../../css/meeting.less';
+import moment from 'moment';
+
